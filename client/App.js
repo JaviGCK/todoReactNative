d90879aa594ea5ba46a143f30cea3bc8cc4e237c
Javier García <javigcpgr@gmail.com>
2024-04-08 15:35:08 +0200

    feat(app.js): async function to watch task

--- client/App.js
+++ client/App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const response = await fetch('http://localhost:8080/todos/1');
+    const data = await response.json();
+    setTodos(data);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView>
+        <FlatList
+          data={todos}
+          keyExtractor={(todo) => todo.id}
+          renderItem={({ item }) => <Text>{item.title}</Text>}
+          ListHeaderComponent={() => <Text style={styles.title}>Today</Text>}
+          contentContainerStyle={styles.contentContainerStyle}
+        />
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +32,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E9E9EF",
+  },
+  contentContainerStyle: {
+    padding: 15,
+  },
+  title: {
+    fontWeight: "800",
+    fontSize: 28,
+    marginBottom: 15,
   },
 });
