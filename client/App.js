92b4b70500b944d14cc6c39543ed90879304251d
Javier García <javigcpgr@gmail.com>
2024-04-09 16:25:20 +0200

    feat(ToDoList.js): atomized app.js

--- client/App.js
+++ client/App.js
@@ -1,64 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { InputTask } from "./components/InputTask";
-import { Task } from "./components/Task";
-import { fetchData } from "./api/todos";
+import TodoList from "./components/ToDoList";
 
-export default function App() {
-  const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  async function fetchTodos() {
-    try {
-      const data = await fetchData();
-      setTodos(data);
-    } catch (error) {
-      console.error("Error fetching todos:", error);
-    }
-  }
-
-  function clearTodo(id) {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  }
-
-  function toggleTodo(id) {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id
-          ? { ...todo, completed: todo.completed === 1 ? 0 : 1 }
-          : todo
-      )
-    );
-  }
-
+export function App() {
   return (
     <GestureHandlerRootView style={styles.flex1}>
       <BottomSheetModalProvider>
         <View style={styles.container}>
-          <SafeAreaView style={styles.flex1}>
-            <FlatList
-              data={todos}
-              keyExtractor={(todo) => todo.id.toString()}
-              renderItem={({ item }) => (
-                <Task
-                  {...item}
-                  toggleTodo={toggleTodo}
-                  clearTodo={clearTodo}
-                  fetchTodos={fetchTodos}
-                />
-              )}
-              ListHeaderComponent={() => (
-                <Text style={styles.title}>Today</Text>
-              )}
-              contentContainerStyle={styles.contentContainerStyle}
-            />
-            <InputTask todos={todos} setTodos={setTodos} />
-          </SafeAreaView>
+          <TodoList />
         </View>
       </BottomSheetModalProvider>
     </GestureHandlerRootView>
@@ -72,13 +24,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#E9E9EF",
-  },
-  contentContainerStyle: {
-    padding: 15,
-  },
-  title: {
-    fontWeight: "800",
-    fontSize: 28,
-    marginBottom: 15,
   },
 });
